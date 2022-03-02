63279d9b9ad3140153a84c5ea74e16e3d8a0e506
UmarBegg <umar97@hotmail.co.uk>
2022-03-02 01:02:22 +0000

    small changes

--- src/App.js
+++ src/App.js
@@ -4,29 +4,28 @@ import styled from "styled-components"
 const Flag = styled.img`
   display: block;
   margin: auto;
-  margin-top: 100px;
-  width: 500px;
+  margin-top: 6rem;
+  width: 36rem;
   border-style: solid;
 `
 
 function App() {
   const [country, setCountry] = useState(null)
   useEffect(() => {
-    const randomNumberInCountryArray = Math.floor(Math.random() * 250)
+    const randomCountry = () => {
+      let num = Math.floor(Math.random() * 250)
+      return num === 53 ? randomCountry() : num
+    }
 
     fetch("https://restcountries.com/v3.1/all")
       .then((response) => response.json())
-      .then((data) => setCountry(data[randomNumberInCountryArray]))
+      .then((data) => setCountry(data[randomCountry()]))
   }, [])
 
   const flag = country ? country.flags.png : null
   const name = country ? country.name.common : null
 
-  return (
-    <div>
-      {country && <Flag src={flag} alt={name} />}
-    </div>
-  )
+  return <div>{country && <Flag src={flag} alt={name} />}</div>
 }
 
 export default App

18f94bfd1136d66acd234a63260da832f1302ba9
UmarBegg <umar97@hotmail.co.uk>
2022-03-02 00:01:27 +0000

    call api and display random flag

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import styled from "styled-components"
+
+const Body = styled.div`
+
+`
 
 function App() {
+  const [country, setCountry] = useState(null)
+  useEffect(() => {
+    const randomNumberInCountryArray = Math.floor(Math.random() * 250)
+
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => setCountry(data[randomNumberInCountryArray]))
+  }, [])
+
+  const flag = country ? country.flags.png : null
+  const name = country ? country.name.common : null
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={flag} alt={name} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
